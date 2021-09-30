9721349ece91a4559b7b8ac798f59b3c85e62364
johnnymcd <johnathanmcdowell24@gmail.com>
2021-09-30 13:26:32 -0700

    dogVotes

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let dogDiv = document.querySelector("#doggo");
+console.log(dogDiv);
+
+// let animals = document.querySelectorAll(".animal")
+
+
+//if(dogvote) {
+//    let dogvotecounter =parseint(dog-votes);
+ //   dogtbrow.textContent = dogvotecounter;
+//}
+
+
+
+
+
+// select pet div
+//let dogDiv = document.querySelector("#doggo");
+
+
+
+
